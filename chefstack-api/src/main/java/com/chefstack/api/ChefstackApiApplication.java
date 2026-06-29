269b18e70f8b8ab5a139969f8f9a787fa2d1b655
Bryamfx11 <bvillalba@uniempresarial.edu.co>
2026-06-28 20:47:16 -0500

    build: configuracion base del backend Spring Boot

---
+++ chefstack-api/src/main/java/com/chefstack/api/ChefstackApiApplication.java
@@ -0,0 +1,12 @@
+package com.chefstack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefstackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChefstackApiApplication.class, args);
+    }
+}
